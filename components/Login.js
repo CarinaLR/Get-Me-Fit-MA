467a7c513841fc25330ebfb963b700244dc4659d
CarinaLR <carinalr84@gmail.com>
2019-11-15 11:31:17 -0500

    login component

---
+++ components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigation } from 'react-navigation;
+
+export default class Login extends React.Component {
+  render (){
+    return (
+
+    )
+  }
+}
